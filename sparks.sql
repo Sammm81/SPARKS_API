6bb38c09d224c746c5d12ec40442d3855ea8b992
Chris1115 <derino1115@gmail.com>
2022-04-05 13:56:41 +0700

    nambahin sql starter

---
+++ sparks.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE sparks;
+
+use sparks;
+
+CREATE TABLE users (
+    id varchar(10) NOT NULL UNIQUE, 
+    name varchar(20) NOT NULL UNIQUE, 
+    email varchar(30) NOT NULL UNIQUE, 
+    password varchar(500) NOT NULL UNIQUE, 
+    admin bool DEFAULT 0 NOT NULL, 
+    balance bigint DEFAULT 0 NOT NULL, 
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE place (
+    id varchar(10) NOT NULL UNIQUE, 
+    place_name varchar(30) NOT NULL UNIQUE, 
+    full bool DEFAULT 0 NOT NULL, 
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE area (
+    id varchar(10) NOT NULL UNIQUE, 
+    place_id varchar(10) NOT NULL, 
+    area_name varchar(30) NOT NULL UNIQUE, 
+    full bool DEFAULT 0 NOT NULL, 
+    PRIMARY KEY (id), 
+    FOREIGN KEY (place_id) REFERENCES place(id)
+); 
+
+CREATE TABLE slot (
+    id varchar(10) NOT NULL UNIQUE, 
+    area_id varchar(10) NOT NULL, 
+    slot_name varchar(30) NOT NULL UNIQUE, 
+    book bool DEFAULT 0 NOT NULL, 
+    available bool DEFAULT 1 NOT NULL, 
+    PRIMARY KEY (id), 
+    FOREIGN KEY (area_id) REFERENCES area(id)
+);
+
+CREATE TABLE book (
+    id varchar(10) NOT NULL UNIQUE, 
+    user_id varchar(10) NOT NULL, 
+    slot_id varchar(10) NOT NULL, 
+    token varchar(20) NOT NULL UNIQUE, 
+    verified bool DEFAULT 0 NOT NULL, 
+    record timestamp DEFAULT CURRENT_TIMESTAMP, 
+    PRIMARY KEY (id), 
+    FOREIGN KEY (user_id) REFERENCES users(id), 
+    FOREIGN KEY (slot_id) REFERENCES slot(id)
+);
+
+CREATE TABLE payment (
+    id varchar(10) NOT NULL UNIQUE, 
+    book_id varchar(10) NOT NULL, 
+    paid bool DEFAULT 0 NOT NULL, 
+    record timestamp DEFAULT CURRENT_TIMESTAMP, 
+    PRIMARY KEY (id)
+);
